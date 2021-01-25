1de14db6b47459870584cc7627c08833eef97512
lsy <lsykk1234@163.com>
2021-01-25 16:03:39 +0800

    会议功能联调前最新版本 添加行程安排前最新版本

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,42 +5,26 @@ let BASE64 = require("../../utils/Base64.js");
 var util = require("../../utils/util");
 import md5 from "../../utils/MD5";
 import Notify from '../../miniprogram_npm/@vant/weapp/notify/notify';
+import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
 var app = getApp();
 
 Page({
   data: {
     username:'',
-    password:''
+    password:'',
+    token:'undefined_token'
   },
-  onLoad: function (options) {},
-  onReady: function () {},
-  onShow: function () {},
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {},
-  // encryptByDESModeEBC: function(message){
-  //   var key = 'BOTWAVEE';
-  //   var keyHex = CryptoJS.enc.Utf8.parse(key);
-  //   var encrypted = CryptoJS.DES.encrypt(message, keyHex, {
-  //   mode: CryptoJS.mode.ECB,
-  //   padding: CryptoJS.pad.Pkcs7
-  //   });
-  //   return encrypted.ciphertext.toString();
-  // },
-  Login: function(){
+  Login_first: function(){
     // console.log(this.data.username,this.data.password)
-    // console.log(this.encryptByDESModeEBC('1111',"rrettre","234234234"))
     if ( this.data.username == '' || this.data.password == ''){
       Notify({ type: 'danger', message: '用户名或密码不能为空!' });
       return false;
     }
     else {
       // this.requestTask();
-      wx.navigateTo({
-        url: '../main/main'
-      })
+      // wx.navigateTo({
+      //   url: '../main/main'
+      // })
       // 接口1 token获取
       // 参数：login:”yzh”, nowdate:”当前日期”, secstr:”加密串”
       // 说明secstr  为  tzh|nowdate 拼接串的  des 加密值
@@ -58,9 +42,9 @@ Page({
       // console.log(secstring);
       // console.log(secstr);
       Notify({ type: 'primary', message: '用户名:'+ this.data.username + '密码:' + this.data.password + 'DES加密之后：'+ secstr + '密钥:' + key});
-      // url: 'http://118.31.73.43:9389/wuchan/wexin/getToken/findAppletActivityInfoDetails.do?id='+id,
 
-    
+      //请求token
+      let _that = this;
       wx.request({
         url: 'https://118.31.73.43:9389/wuchan/wexin/getToken',
         method: "post",
@@ -73,89 +57,66 @@ Page({
           'content-type': 'application/json' // 默认值
         },
         success (res) {
+          console.log("获取token成功")
           console.log(res.data)
+          var newtoken = 'newtoken';
+          //更新全局token
+          _that.setData({
+            token : newtoken
+          })
+          _that.Login_second();
+        },
+        fail (res) {
+          console.log("获取token失败");
+          _that.Login_second(); //最后要注释掉
         }
       })
-      // this.$http.get(
-      //   'http://118.31.73.43:9389/wuchan/wexin/getToken',
-      //   {
-      //       params: {
-      //         login: login,  //这里是发送给服务器的参数（参数名：参数值）  
-      //         nowdate: nowdate,
-      //         secstr: secstr 
-      //       },
-      //   }).then((res)=>{
-      //       console.log(res)
-      //   });
-
-    //   var that = this //创建一个名为that的变量来保存this当前的值  
-    //   wx.request({  
-    //      url: 'http://118.31.73.43:9389/wuchan/wexin/getToken',  //接口
-    //      method: 'post',  
-    //      data: {  
-    //       login: login,  //这里是发送给服务器的参数（参数名：参数值）  
-    //       nowdate: nowdate,
-    //       secstr: secstr 
-    //      },  
-    //      header: {  
-    //        'content-type': 'application/x-www-form-urlencoded'  //这里注意POST请求content-type是小写，大写会报错  
-    //      },  
-    //      success: function (res) {  
-    //       //  that.setData({ //这里是修改data的值  
-    //       //    test: res.data //test等于服务器返回来的数据  
-    //       //  });  
-    //        console.log(res)  
-    //      }  
-    //  });
-      // app.apiGet({
-      //   url: '接口/findAppletActivityInfoDetails.do?id='+id,
-      //   data: {
-      //     id,
-      //     login,
-      //     nowdate,
-      //     secstr
-          
-      //   }
-      // }).then((res)=>{
-      //   console.log(res);
-      //   wx.navigateTo({
-      //     url: '../main/main'
-      //   })
-      // })
-      // wx.request( {
-      //   url: "http://118.31.73.43:9389/wuchan/wexin/getToken/findAppletActivityInfoDetails.do?login=+id?key=我的appkey&from=CNY&to="+code,
-      //   success: function( res ) {
-      //     that.setData( {
-      //       currencyF_Name: res.data.result[0].currencyF_Name,
-      //       currencyT_Name: res.data.result[0].currencyT_Name,
-      //       currencyF: res.data.result[0].currencyF,
-      //       currencyT: res.data.result[0].currencyT,
-      //       currencyFD: res.data.result[0].currencyFD,
-      //       exchange: res.data.result[0].exchange,
-      //       result: res.data.result[0].result,
-      //       updateTime: res.data.result[0].updateTime,
-      //     })
-      //   }
-      // })
-
-      
-      // 登录接口
-      //参数 login:this.data.username ,userpassword: ”MD5加密”, token:
-      //MD5加密 
-      var darkpassword = md5.hexMD5(this.data.password);
-      console.log(darkpassword);
-      Notify({ type: 'primary', message: '用户名:'+ this.data.username + '密码:' + this.data.password + 'MD5加密之后：'+ darkpassword });
-      //接口函数 this.data.username darkpassword token
-      //http://118.31.73.43:9389/wuchan/wexin/verification
-
-      //成功则跳转
-      // wx.navigateTo({
-      //   url: '../main/main'
-      // })
-      //传token过去 会议详情接口要用
-
     }
   },
+  Login_second() {
+    console.log("进入Login_second函数");
+    // 登录接口
+    //参数 login:this.data.username ,userpassword: ”MD5加密”, token:
+    //MD5加密 
+    //接口函数 this.data.username darkpassword token
+    //http://118.31.73.43:9389/wuchan/wexin/verification
+    const darkpassword = md5.hexMD5(this.data.password);
+    console.log(darkpassword);
+    Notify({ type: 'primary', message: '用户名:'+ this.data.username + '密码:' + this.data.password + 'MD5加密之后：'+ darkpassword });
+
+    let _that = this;
+    wx.request({
+      url: 'https://118.31.73.43:9389/wuchan/wexin/verification',
+      method: "post",
+      data: {
+          login: _that.data.username,
+          userpassword: darkpassword,
+          token: _that.data.token
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log("登录成功")
+        console.log(res.data)
+        Toast.success('登录成功!');
+        //跳转页面
+        var username= _that.data.username;
+        wx.navigateTo({
+          url: '../main/main?username='+ username ,
+        })
+      },
+      fail (res) {
+        console.log("登录失败");
+        console.log(res.data)
+        //最后要注释掉
+        var username= _that.data.username;
+        wx.navigateTo({
+          url: '../main/main?username='+ username ,
+        })
+      }
+    })
+  }
   /* 获取数据
 * @param api: 请求路由
 * @param params: 查找数据
--- pages/main/main.js
+++ pages/main/main.js
@@ -1,5 +1,7 @@
 var util = require("../../utils/util");
-var utils = require("../../utils/time-utils")
+var utils = require("../../utils/time-utils");
+let BASE64 = require("../../utils/Base64.js");
+let DES3 = require("../../utils/DES3.js");
 
 Page({
   data: {
@@ -9,7 +11,7 @@ Page({
     Conference_list: [],
     thirty_b:[],
     thirty_s:[],
-    thirty_eight_b:[],
+    thirty_eight_s:[],
     shanghai_b:[],
     date: '',
     show: false,
@@ -44,165 +46,236 @@ Page({
       {
         time: '17:30'
       }
-    ]
+    ],
+    user_token: '',
+    username:'',
+    Request_date:''
   },
-  onLoad: function (options) {
+  //接收username
+  onLoad: function (option) {
     //默认先获取当天的会议列表
-    //页面跳转传值 接收token
-    //this.setData({ }) 
-
+    //页面跳转传值 接收username
+    console.log(option);
+    //更新全局username值
+    this.setData({
+      username: option.username
+    });
+    //请求当天会议信息
     this.onRequest();
   },
+  //获取token
   onRequest:function(){
     //获取数据 赋值
-    var nowdate = util.formatTime(new Date()) ; //当前日期
-    //格式处理成 2020-01-20 格式
+    // const nowdate = util.formatTime(new Date()) ; //当前日期
+    // const nowdate_deal = nowdate.substr(0,10).replace(new RegExp("/","gm"),"-")
+    // console.log(nowdate_deal);
+    // 获取token
+    let _that = this;
+    const key = 'A1B2C3D4E5F60708'; //密钥
+    const nowdate = util.formatTime(new Date()) ; //当前日期
+    const nowdate_deal = nowdate.substr(0,10).replace(new RegExp("/","gm"),"-")
+    console.log(nowdate_deal);
+    this.setData({
+      Request_date: nowdate_deal
+    })
+    const secstring = _that.data.username + '|' + nowdate ; //拼接串
+    const secstr = BASE64.encoder(DES3.encrypt(key, secstring));//加密串
+    //请求token
+    wx.request({
+        url: 'https://118.31.73.43:9389/wuchan/wexin/getToken',
+        method: "post",
+        data: {
+            login: _that.data.username,
+            nowdate: nowdate,
+            secstr: secstr 
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          console.log("获取token成功")
+          console.log(res.data)
+          var newtoken = 'newtoken';
+          //更新全局token值
+          _that.setData({
+            user_token: newtoken
+          });
+          _that.onGetConfList();
+        },
+        fail (res) {
+          console.log("获取token失败");
+          //最后要注释掉
+          _that.onGetConfList();
+        }
+    })
+
+  },
+  //获取会议室列表
+  onGetConfList: function() {
     //接口函数
     //接口地址：http://118.31.73.43:9389/wuchan/wexin/meeting
     //参数值：{token:,startdate:”2020-01-20”,enddate:”2020-01-25”}
-
-
-
-
     //会议列表赋值给Conference_list
-
     let datas = [
-  {
-        id:'001',
-        room: "30#大",
-        title: "财务部",
-        name: "财务部年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 10:00:00",
-        e_time:"2021-01-21 11:30:00",
-        attendee:['BBB','BBB','BBB','BBB','BBB','BBB'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      }, {
-        id:'002',
-        room: "30#大",
-        title: "综合部",
-        name: "综合部年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 13:30:00",
-        e_time:"2021-01-21 16:30:00",
-        attendee:['AAA','AAA','AAA','AAA','AAA','AAA'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      }, 
       {
-        id:'003',
-        room: "30#小",
-        title: "信息部",
-        name: "信息部年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 08:45:00",
-        e_time:"2021-01-21 09:30:00",
-        attendee:['CCC','CCC','CCC','CCC','CCC','CCC'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      },{
-        id:'004',
-        room: "38#大",
-        title: "财务部",
-        name: "财务部年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 10:45:00",
-        e_time:"2021-01-21 11:30:00",
-        attendee:['DDD','DDD','DDD','DDD','DDD','DDD'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      }, {
-        id:'005',
-        room: "上海#大",
-        title: "综合部",
-        name: "综合部年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 13:30:00",
-        e_time:"2021-01-21 16:30:00",
-        attendee:['EEE','EEE','EEE','EEE','EEE','EEE'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      }, {
-        id:'006',
-        room: "38#大",
-        title: "全体",
-        name: "全体年度总结会议",
-        people: "赟总",
-        s_time:"2021-01-21 17:00:00",
-        e_time:"2021-01-21 17:30:00",
-        attendee:['FFF','FFF','FFF','FFF','FFF','FFF'],
-        length:"",
-        m_top:'',
-        titlem_top:''
-      }
-    ];
-    // console.log(datas)
-    this.setData({
-      Conference_list : datas
+          id:'001',
+          room: "30#大",
+          title: "财务部",
+          name: "财务部年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 10:00:00",
+          e_time:"2021-01-21 11:30:00",
+          attendee:['BBB','BBB','BBB','BBB','BBB','BBB'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        }, {
+          id:'002',
+          room: "30#大",
+          title: "综合部",
+          name: "综合部年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 13:30:00",
+          e_time:"2021-01-21 16:30:00",
+          attendee:['AAA','AAA','AAA','AAA','AAA','AAA'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        }, 
+        {
+          id:'003',
+          room: "30#小",
+          title: "信息部",
+          name: "信息部年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 08:45:00",
+          e_time:"2021-01-21 09:30:00",
+          attendee:['CCC','CCC','CCC','CCC','CCC','CCC'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        },{
+          id:'004',
+          room: "38#大",
+          title: "财务部",
+          name: "财务部年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 10:45:00",
+          e_time:"2021-01-21 11:30:00",
+          attendee:['DDD','DDD','DDD','DDD','DDD','DDD'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        }, {
+          id:'005',
+          room: "上海#大",
+          title: "综合部",
+          name: "综合部年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 13:30:00",
+          e_time:"2021-01-21 16:30:00",
+          attendee:['EEE','EEE','EEE','EEE','EEE','EEE'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        }, {
+          id:'006',
+          room: "38#大",
+          title: "全体",
+          name: "全体年度总结会议",
+          people: "赟总",
+          s_time:"2021-01-21 17:00:00",
+          e_time:"2021-01-21 17:30:00",
+          attendee:['FFF','FFF','FFF','FFF','FFF','FFF'],
+          length:"",
+          m_top:'',
+          titlem_top:''
+        }
+      ];
+
+    let _that = this;
+    wx.request({
+        url: 'https://118.31.73.43:9389/wuchan/wexin/meeting',
+        method: "post",
+        data: {
+          token: _that.data.user_token,
+          startdate: _that.data.Request_date,
+          enddate: _that.data.Request_date
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success (res) {
+          console.log("获取token成功");
+          _that.setData({
+            Conference_list : datas
+          });
+          _that.onListDeal();
+        },
+        fail (res) {
+          console.log("获取token失败");
+          //最后要注释掉
+          _that.setData({
+            Conference_list : datas
+          });
+          _that.onListDeal();
+        }
     })
-    // console.log( this.data.Conference_list.length)
-    //修改Conference_list的length和m_top属性
-    for ( var i = 0 ; i < this.data.Conference_list.length ; i++) {
-      var chuo_s = new Date(this.data.Conference_list[i].s_time.replace(/-/g,"/")).getTime()
-      var chuo_e = new Date(this.data.Conference_list[i].e_time.replace(/-/g,"/")).getTime()
-      var length_num = (chuo_e - chuo_s) / 30000 ;
-      // console.log(chuo_s);
-      // console.log(chuo_e - chuo_s);
-      // console.log((chuo_s - 1611189000000 )/ 324000);
-      var length_num = (chuo_e - chuo_s) / 30000 ;
-      var m_top_num = (chuo_s - 1611189000000 )/ 324000 * 2.6 ;
-      var titlem_top_num = length_num * 0.4;
-      if ( length_num <= 100) {
-        titlem_top_num =  length_num * 0.1;
+  },
+  onListDeal:function() {
+    //修改Conference_list的length和m_top属性和titlem_top属性
+      for ( var i = 0 ; i < this.data.Conference_list.length ; i++) {
+        var chuo_s = new Date(this.data.Conference_list[i].s_time.replace(/-/g,"/")).getTime()
+        var chuo_e = new Date(this.data.Conference_list[i].e_time.replace(/-/g,"/")).getTime()
+        var length_num = (chuo_e - chuo_s) / 30000 ;
+        // console.log(chuo_s);
+        // console.log(chuo_e - chuo_s);
+        // console.log((chuo_s - 1611189000000 )/ 324000);
+        var length_num = (chuo_e - chuo_s) / 30000 ;
+        var m_top_num = (chuo_s - 1611189000000 )/ 324000 * 2.6 ;
+        var titlem_top_num = length_num * 0.4;
+        if ( length_num <= 100) {
+          titlem_top_num =  length_num * 0.2;
+        }
+        if ( length_num <= 60) {
+          titlem_top_num =  length_num * 0.1;
+        }
+        var length_string = length_num.toString()+"px";
+        var m_top_string = m_top_num.toString() + "%";
+        var titlem_top_string = titlem_top_num.toString() + "px";
+        const state1 = "Conference_list["+ i +"].length"
+        const state2 = "Conference_list["+ i +"].m_top"
+        const state3 = "Conference_list["+ i +"].titlem_top"
+        this.setData({
+          [state2]: m_top_string,
+          [state1]: length_string,
+          [state3]: titlem_top_string,
+        });
       }
-      var length_string = length_num.toString()+"px";
-      var m_top_string = m_top_num.toString() + "%";
-      var titlem_top_string = titlem_top_num.toString() + "px";
-      const state1 = "Conference_list["+ i +"].length"
-      const state2 = "Conference_list["+ i +"].m_top"
-      const state3 = "Conference_list["+ i +"].titlem_top"
+      //分组 根据会议室不同将Conference_list分成四个数组
+      for ( var i = 0 ; i < this.data.Conference_list.length ; i++) {
+        if ( this.data.Conference_list[i].room == '30#大')
+          this.data.thirty_b.push(this.data.Conference_list[i]);
+        else if ( this.data.Conference_list[i].room == '30#小' ) 
+          this.data.thirty_s.push(this.data.Conference_list[i]);
+        else if ( this.data.Conference_list[i].room == '38#大' )
+          this.data.thirty_eight_s.push(this.data.Conference_list[i]);
+        else
+          this.data.shanghai_b.push(this.data.Conference_list[i]);
+      }
+      //更新四个会议室数组
       this.setData({
-        [state2]: m_top_string,
-        [state1]: length_string,
-        [state3]: titlem_top_string,
+        thirty_b : this.data.Conference_list,
+        thirty_s : this.data.Conference_list,
+        thirty_eight_s : this.data.Conference_list,
+        shanghai_b : this.data.Conference_list
       });
-    }
-    //分组 根据会议室不同将Conference_list分成四个数组
-    for ( var i = 0 ; i < this.data.Conference_list.length ; i++) {
-      if ( this.data.Conference_list[i].room == '30#大')
-        this.data.thirty_b.push(this.data.Conference_list[i]);
-      else if ( this.data.Conference_list[i].room == '30#小' ) 
-        this.data.thirty_s.push(this.data.Conference_list[i]);
-      else if ( this.data.Conference_list[i].room == '38#大' )
-        this.data.thirty_eight_b.push(this.data.Conference_list[i]);
-      else
-        this.data.shanghai_b.push(this.data.Conference_list[i]);
-    }
-    // console.log(this.data.thirty_b);
-    this.setData({
-      thirty_b : datas,
-      thirty_s : datas,
-      thirty_eight_b : datas,
-      shanghai_b : datas
-    });
   },
   onReady: function () {
     this.setData({
       timeBean: utils.getWeekDayList(this.data.selectWeek)
     })
   },
-  onShow: function () {},
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {},
   onDisplay() {
     this.setData({ show: true });
   },
@@ -240,8 +313,6 @@ Page({
   },
   Tapiteminfo: function(e){
     //根据id 筛选出会议 显示会议详情
-    //或者后端给一个接口 传入id 显示会议详情
-
     // console.log("点击查看详情")
     console.log(e.currentTarget.dataset.id)
     var iteminfo_object = this.data.Conference_list.find(item => item.id == e.currentTarget.dataset.id)
@@ -262,7 +333,14 @@ Page({
     this.setData({
       timeBean,
       selectWeek
+    });
+    // console.log(this.data.timeBean);
+    // console.log(this.data.selectWeek);
+    this.setData({
+      Request_date: this.data.timeBean.yearMonth + '-' + this.data.timeBean.weekDayList[this.data.timeBean.selectDay].day
     })
+    console.log(this.data.Request_date);
+    this.onRequest();
   },
   //下一周
   nextWeek:function(e){
@@ -276,6 +354,13 @@ Page({
       timeBean,
       selectWeek
     })
+    // console.log(this.data.timeBean);
+    // console.log(this.data.selectWeek);
+    this.setData({
+      Request_date: this.data.timeBean.yearMonth + '-' + this.data.timeBean.weekDayList[this.data.timeBean.selectDay].day
+    })
+    console.log(this.data.Request_date);
+    this.onRequest();
   },
   //切换日期
   dayClick:function(e){
@@ -286,9 +371,12 @@ Page({
     })
     // console.log(e)
     // console.log(this.data.timeBean.yearMonth)
-    console.log(this.data.timeBean.yearMonth + '-' + this.data.timeBean.weekDayList[this.data.timeBean.selectDay].day)
-  },
+    // console.log(this.data.timeBean.yearMonth + '-' + this.data.timeBean.weekDayList[this.data.timeBean.selectDay].day)
+    this.setData({
+      Request_date: this.data.timeBean.yearMonth + '-' + this.data.timeBean.weekDayList[this.data.timeBean.selectDay].day
+    })
+    console.log(this.data.Request_date);
+    this.onRequest();
 
-
-
+  }
 })
